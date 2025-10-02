5fa1e52ab6e96643edd394088bc5fae18644aa40
mitchell <mitchell@fiveonefour.com>
2025-10-02 15:50:29 -0600

    align moose with lastest project style

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-pass
+from moose_lib import Task, TaskConfig, TaskContext, Workflow, WorkflowConfig
+from pydantic import BaseModel
+ 
+class Foo(BaseModel):
+  name: str;
+ 
+def run_task1(ctx: TaskContext[Foo]) -> None:
+  name = ctx.input.name or "world"
+  greeting = f"hello, {name}!"
+ 
+task1 = Task[Foo, None](
+  name="task1",
+  config=TaskConfig(run=run_task1)
+)
+ 
+myworkflow = Workflow(
+  name="myworkflow",
+  config=WorkflowConfig(starting_task=task1)
+)
